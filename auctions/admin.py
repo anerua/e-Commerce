54ef507f4feaef96dec7afe8215fb0119e058714
Martins Anerua <aneruamartins@gmail.com>
2021-07-16 14:57:17 +0100

    category and image link optional for listings

--- auctions/admin.py
+++ auctions/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import AuctionListing, Bid, Comment
+
 # Register your models here.
+
+admin.site.register(AuctionListing)
+admin.site.register(Bid)
+admin.site.register(Comment)
--- auctions/forms.py
+++ auctions/forms.py
@@ -4,8 +4,8 @@ class ListingForm(forms.Form):
     title = forms.CharField(label='Title', max_length=64)
     description = forms.CharField(label='Description')
     starting_bid = forms.IntegerField(max_value=999999999999)
-    image = forms.URLField()
-    category = forms.CharField(max_length=64)
+    image = forms.URLField(help_text="Image URL", required=False)
+    category = forms.CharField(max_length=64, required=False)
 
 
 class BiddingForm(forms.Form):
